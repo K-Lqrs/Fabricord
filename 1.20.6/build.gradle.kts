0d4d7260f402bc297b3c494447370b3961ede107
Ruxy <tomatotouroku0001@gmail.com>
2024-06-29 18:37:43 +0900

    Add something

--- 1.20.6/build.gradle.kts
+++ 1.20.6/build.gradle.kts
@@ -1,18 +1,32 @@
 plugins {
-    id("java")
+    id("org.jetbrains.kotlin.jvm")
+    id("fabric-loom")
 }
 
 group = "net.rk4z.fabricord"
-version = "1.0-SNAPSHOT"
+version = "4.0-1.20.6"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    val minecraftVersion: String by project
+    val mappingsVersion: String by project
+    val loaderVersion: String by project
+    val fabricVersion: String by project
 
-}
+    minecraft("com.mojang:minecraft:$minecraftVersion")
+    mappings("net.fabricmc:yarn:$mappingsVersion")
+    modImplementation("net.fabricmc:fabric-loader:$loaderVersion")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:$fabricVersion")
 
-tasks.test {
-    useJUnitPlatform()
+    implementation("net.rk4z:beacon:1.2.5")
+    implementation("net.dv8tion:JDA:5.0.0-beta.24"){
+        exclude("net.java.dev.jna", "jna")
+    }
+
+    implementation("org.yaml:snakeyaml:2.0")
+    implementation("club.minnced:discord-webhooks:0.8.4")
+    implementation("net.kyori:adventure-text-serializer-gson:4.14.0")
 }
--- 1.20.6/settings.gradle.kts
+++
@@ -1,2 +0,0 @@
-rootProject.name = "1.20.6"
-
